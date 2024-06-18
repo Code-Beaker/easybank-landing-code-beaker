245bf445e6789b27d87cc08e1eb6ad9b7ff7e880
Tharun Raj <tharun.2.linux@gmail.com>
2024-06-18 10:36:07 +0530

    create hamburger menu

--- app/main.js
+++ app/main.js
@@ -1,2 +1,16 @@
 import "./scss/style.scss";
 import "./typeScales.css";
+
+const navtoggleButton = document.getElementById("navToggleButton");
+const navigationLinks = document.querySelector(".navbar__links");
+const navigationToggleIcon = document.querySelector(".navbar__toggle-icon");
+
+navtoggleButton.addEventListener("click", () => {
+  navigationLinks.classList.toggle("links-show");
+
+  if (navigationLinks.classList.contains("links-show")) {
+    navigationToggleIcon.src = "images/icon-close.svg";
+  } else {
+    navigationToggleIcon.src = "images/icon-hamburger.svg";
+  }
+});
